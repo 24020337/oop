3019705e5392a4dd6ee3c92a2516912c64959d79
01010100Tr <24020337@vnu.edu.vn>
2025-09-23 15:54:53 +0700

    introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyen Thanh Trung\t24020337\t24020337\t24020337@vnu.edu.vn");
+        for (int i = 9; i > 0;i--) {
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+    }
+}
