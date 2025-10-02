2299ec6335b72c67010a819bd3429f8304bf004b
24020337 <24020337@vnu.edu.vn>
2025-10-02 20:27:12 +0700

    CharGrid

--- HW2/src/main/java/CharGrid.java
+++ HW2/src/main/java/CharGrid.java
@@ -11,15 +11,7 @@ public class CharGrid {
 	 * @param grid
 	 */
 	public CharGrid(char[][] grid) {
-        this.grid = new char[grid.length][];
-        for (int i = 0; i < grid.length; i++) {
-            this.grid[i] = new char[grid[i].length];
-        }
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                this.grid[i][j] = grid[i][j];
-            }
-        }
+        this.grid = grid;
 	}
 	
 	/**
@@ -28,7 +20,24 @@ public class CharGrid {
 	 * @return area for given char
 	 */
 	public int charArea(char ch) {
-		return 0; // YOUR CODE HERE
+        int minCol = Integer.MAX_VALUE;
+        int maxCol = Integer.MIN_VALUE;
+        int minRow = Integer.MAX_VALUE;
+        int maxRow = Integer.MIN_VALUE;
+        for  (int i = 0; i < this.grid.length; i++) {
+            for(int j = 0; j < this.grid[i].length; j++) {
+                if(this.grid[i][j] == ch) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+        if (minCol == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return (maxCol-minCol+1)*(maxRow-minRow+1); // YOUR CODE HERE
 	}
 	
 	/**
@@ -36,7 +45,35 @@ public class CharGrid {
 	 * @return number of + in grid
 	 */
 	public int countPlus() {
-		return 0; // YOUR CODE HERE
-	}
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                char c = grid[i][j];
+
+                int up = countArm(i, j, -1, 0, c);
+                int down = countArm(i, j, 1, 0, c);
+                int left = countArm(i, j, 0, -1, c);
+                int right = countArm(i, j, 0, 1, c);
+
+                int min = Math.min(Math.min(up, down), Math.min(left, right));
+
+                if (min >= 1) count++;
+            }
+        }
+        return count;// YOUR CODE HERE
+    }
+
+    private int countArm(int i, int j, int di,int dj,char ch) {
+        int len = 0;
+        i += di; j += dj;
+        while (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == ch) {
+            len++;
+            i += di; j += dj;
+        }
+        return len;
+    }
 	
 }
